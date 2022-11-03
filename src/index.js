5f688605790b8f7cc60ab9440436ccb747abcebd
Jordan Flash <flash.jordan.3@gmail.com>
2022-11-03 14:05:58 +0000

    random float and coin flip added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ export function printMsg() {
 }
 // These functions are for numbers
 export function randomInt(min, max) {
-
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(randomInt(1, 100));
+export function randomFloat(min, max, decimal) {
+  const ran = Math.random() * (max - min) + min;
+  return Math.round(ran * Math.pow(10, decimal)) / Math.pow(10, decimal);
+}
+
+export function coinFlip() {
+  const coin = Math.random();
+  if (coin > 0.5) {
+    return true;
+  }
+  if (coin < 0.5) {
+    return false;
+  }
+  if ((coin = 0.5)) {
+    return "Damn, it landed on its side, how unlucky";
+  }
+}
+
+console.log(coinFlip());
+
+/*
+random int ✅
+random float ✅
+coin flip
+random array
+random object
+
+
+*/
