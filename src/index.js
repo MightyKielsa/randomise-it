5e854d6571b47eb742398ff3067f4288ad9161de
Jordan Flash <flash.jordan.3@gmail.com>
2022-11-03 13:41:15 +0000

    first random number generator made

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 export function printMsg() {
-    console.log("This is a message from the demo package");
-  };
+  console.log("This is a message from the demo package");
+}
+// These functions are for numbers
+export function randomInt(min, max) {
+  // math.random generating a random number greater than min and smaller than max
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
+console.log(randomInt(1, 100));
