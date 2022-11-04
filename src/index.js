b1d332457d35b7f76bb592b84427e8b0dd69fb17
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2022-11-04 14:40:03 +0000

    randomBoolArr, randomColor, randomLetter are done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-export function printMsg() {
-  console.log("This is a message from the demo package");
-}
 // These functions are for numbers
 export function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -11,7 +8,7 @@ export function randomFloat(min, max, decimal) {
   return Math.round(ran * Math.pow(10, decimal)) / Math.pow(10, decimal);
 }
 
-export function coinFlip() {
+export function randomBool() {
   const coin = Math.random();
 
   if (coin < 0.5) {
@@ -38,7 +35,90 @@ export function randomArr(length, min, max, decimal = 0) {
   return arr;
 }
 
-console.log(randomArr(10, 1, 10));
+export function randomBoolArr(length) {
+  let arr = [];
+  if (length > 0) {
+    for (let i = 0; i < length; i++) {
+      arr.push(randomBool());
+    }
+  } else {
+    console.error(
+      "Please check length value. It should be a number larger than 0"
+    );
+  }
+  return arr;
+}
+
+export function randomColor(isRgb = false) {
+  if (isRgb === false) {
+    const colorLetters = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+    ];
+    let hex = "#";
+
+    for (let i = 0; i < 6; i++) {
+      let n = randomInt(0, colorLetters.length);
+      hex += colorLetters[n];
+    }
+    return hex;
+  } else if (isRgb === true) {
+    const rgbArr = randomArr(3, 0, 255);
+    return `rgb(${rgbArr[0]},${rgbArr[1]},${rgbArr[2]})`;
+  } else {
+    console.error("Please check isRgb value. It should be a boolean or empty.");
+  }
+}
+
+export function randomLetter(isUppercase = false) {
+  const letterArr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  if (isUppercase === false) {
+  }
+}
+
+console.log(randomColor("fasdfa"));
 
 /*
 random int ✅
